013c65a4ff396f05371bcb2e1558ddb463ec43ea
Kale Leach <kleach1212@gmail.com>
2022-08-25 19:59:44 -0700

    Commit 5: Resizing for mobile use

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 import "./Header.css"
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Header(){
+  const bars = <FontAwesomeIcon icon={faBars}/> ;
   return (
     <div className = "header">
-      <nav>
+      <DropdownButton id="dropdown-basic-button" title = {bars}>
         {/* <a className= "navlinks"> Contact Us </a> */}
-        <a href = '#RocaList' className= "navlinks"> Roca </a>
-        <a href = '#OurStory' className= "navlinks"> Our Story </a>
-        <a href = '#intro' className= "navlinks"> Home </a>
-      </nav>
+        <Dropdown.Item href = '#RocaList' className= "navlinks"> Roca </Dropdown.Item>
+        <Dropdown.Item href = '#OurStory' className= "navlinks"> Our Story </Dropdown.Item>
+        <Dropdown.Item href = '#intro' className= "navlinks"> Home </Dropdown.Item>
+      </DropdownButton>
+
     </div>
   )
 }
